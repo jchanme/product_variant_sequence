8637602a13159f2acf1c9e60c6eab4daf9bcd6e7
jchanme <39323414+jchanme@users.noreply.github.com>
2019-05-10 22:31:28 +0800

    Update models.py

--- models/models.py
+++ models/models.py
@@ -2,14 +2,9 @@
 
 from odoo import models, fields, api
 
-# class product_variant_sequence(models.Model):
-#     _name = 'product_variant_sequence.product_variant_sequence'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class many2many_product_variant_sequence(models.Model):
+    _name = 'product.variant.handle'
+    _description = 'Model for many2many (handle)'
+    _order = 'sequence'
+    name = fields.Char('Naam')
+    sequence = fields.Integer('sequence', help="Sequence for the handle.",default=10)
