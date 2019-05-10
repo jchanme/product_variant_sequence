e62aa3b183683705b4c3bc93e327ea872a317d08
jchanme <39323414+jchanme@users.noreply.github.com>
2019-05-10 21:52:16 +0800

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -3,33 +3,30 @@
     'name': "product_variant_sequence",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Add handle widge of sequence to the variant list in product template""",
 
     'description': """
-        Long description of module's purpose
+        Add handle widge of sequence to the variant list in product template
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Safyric Co., Ltd.",
+    'website': "https://www.safyric.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Sales',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': [
+        'base',
+        'product',
+    ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+        'views/product_template_view.xml',
     ],
 }
