53cfca2d6000f3cd2fe1fe95738710d99ca5ea36
jchanme <39323414+jchanme@users.noreply.github.com>
2019-05-11 17:27:33 +0800

    Update models.py

--- models/models.py
+++ models/models.py
@@ -3,7 +3,6 @@
 from odoo import models, fields, api
 
 class many2many_product_variant_sequence(models.Model):
-    _inherit = "product.attribute"
-    
-    sequence = fields.Integer('Sequence', help="Determine the display order")
-    attribute_line_ids = fields.many2many('product.attribute.line', 'attribute_id', 'Lines')
+    _inherit = "product.template"
+
+    attribute_line_ids = fields.Many2many('product.attribute.line', 'product_tmpl_id', 'Product Attributes')
